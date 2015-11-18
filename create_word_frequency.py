eee2e2a6dc59f0ded4d8643937dc4521d625aa6d
danil.gizdatullin <gizdatullindanil@gmail.com>
2015-11-18 12:04:20 +0300

    add lemma frequency

--- create_word_frequency.py
+++ create_word_frequency.py
@@ -1,1 +1,21 @@
-create_word_frequency
+import nltk
+import pickle
+
+from nltk.corpus import brown
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+porter_stemmer = PorterStemmer()
+wordnet_lemmatizer = WordNetLemmatizer()
+# wordnet_lemmatizer.lemmatize
+
+all_words_lemms = []
+cnt = 0
+current = 1
+for cat in brown.categories():
+    print(current)
+    current += 1
+    category_words = brown.words(categories=cat)
+    all_words_lemms += [wordnet_lemmatizer.lemmatize(word.lower()) for word in category_words]
+fdist = nltk.FreqDist(all_words_lemms)
+pickle.dump(fdist, open('lemma_frequency/brown_fdist', 'wb'))
