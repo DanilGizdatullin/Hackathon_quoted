e1b8fb1e965df13dadf7876f815e456fa9f6c30d
danil.gizdatullin <gizdatullindanil@gmail.com>
2015-11-19 09:43:28 +0300

    changed .csv data

--- complexity_and_check.py
+++ complexity_and_check.py
@@ -12,7 +12,7 @@ fdist = pickle.load(open('lemma_frequency/brown_fdist', 'rb'))
 wordnet_lemmatizer = WordNetLemmatizer()
 
 
-def check_quote(quote, min_length=20, max_length=140, one_sentence=True, banned_symbols='[]@<>=', no_nnps=True):
+def check_quote(quote, min_length=40, max_length=140, one_sentence=True, banned_symbols='[]@<>=', no_nnps=True):
    if len(quote) < min_length or len(quote) > max_length:
        # print 'Wrong len'
        return False
@@ -32,7 +32,10 @@ def sentence_complexity(sentence, frequency_dist=fdist):
     word_tokenized = nltk.word_tokenize(sentence)
     word_tokenized_filtered = []
     for i in word_tokenized:
-        if not('\'' in i) and not(',' in i) and not('.' in i) and not('?' in i) and not('!' in i) and not(':' in i):
+        if not('\'' in i) and not(',' in i) and not('.' in i) and not('?' in i) \
+                and not('!' in i) and not(':' in i) and not('-' in i)\
+                and not('"' in i) and not('(' in i) and not(')' in i)\
+                and not(i.isdigit()) and not('—' in i) and not('``' in i):
             word_tokenized_filtered.append(i)
 
     word_tags = nltk.pos_tag(word_tokenized_filtered)
@@ -43,7 +46,11 @@ def sentence_complexity(sentence, frequency_dist=fdist):
         else:
             word_lemmatized.append(wordnet_lemmatizer.lemmatize(i[0].lower()))
 
+    word_lemmatized = list(set(word_lemmatized))
     word_frequency = map(lambda x: frequency_dist[x], word_lemmatized)
+    print(sentence)
+    print(word_lemmatized)
+    print(word_frequency)
     word_frequency.sort()
     # print(word_frequency[0:3])
     complexity = 1 / float(sum(word_frequency[0:3]))
@@ -55,6 +62,9 @@ def sentence_complexity(sentence, frequency_dist=fdist):
 
     return complexity
 
+# def check_all_the_nouns(array):
+#     for
+
 if __name__ == "__main__":
     sentence = [0, 0, 0, 0, 0]
     sentence[0] = 'Never trust a nigger: their minds and hair are full of kinks in equal measure'
@@ -62,6 +72,7 @@ if __name__ == "__main__":
     sentence[2] = 'A man of genius makes no mistakes. His errors are volitional and are the portals of discovery'
     sentence[3] = 'Of children as of procreation the pleasure momentary, the posture ridiculous, the expense damnable.'
     sentence[4] = 'The most bitter insult one can offer to a Londoner is ""bastard"" — which", taken for what it means," is hardly an insult at all.'
-    for i in xrange(5):
-        if check_quote(sentence[i]):
-            print(sentence_complexity(sentence[i], fdist))
+    for j in xrange(5):
+        print sentence[j]
+        if check_quote(sentence[j]):
+            print(sentence_complexity(sentence[j], fdist))
--- compute_all_quotes_complexity.py
+++ compute_all_quotes_complexity.py
@@ -5,6 +5,7 @@ from complexity_and_check import check_quote
 from complexity_and_check import sentence_complexity
 
 sentence_id = 1
+# cntr = 0
 f_write = open('data/table_for_db.csv', 'w')
 with open('data/all_quotes.csv', 'r') as f_read:
     for line in f_read:
@@ -14,12 +15,15 @@ with open('data/all_quotes.csv', 'r') as f_read:
                 try:
                     complexity = sentence_complexity(sentence)
                     f_write.write(str(sentence_id)+'\n'+sentence+'\n'+'authors'+'\n'+str(complexity)+'\n')
+                    # cntr += 1
                 except:
                     print('Type 1')
                     print(sentence)
         except:
             print('Type 2')
             print(sentence)
+        # if cntr == 100:
+        #     break
         sentence_id += 1
         # if sentence_id == 87:
         #     break
